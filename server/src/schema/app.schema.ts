37817d763fcd43cdc7197443ac8d7161e7909438
Prithivi <prithivi@users.noreply.github.com>
2026-09-25 11:44:05 +0545

    feat: integrate AWS S3 SDK for file upload, deletion, and signed URL generation

--- server/src/schema/app.schema.ts
+++ server/src/schema/app.schema.ts
@@ -4,4 +4,11 @@ export const appConfigSchema = z.object({
   PORT: z.coerce.number().default(3000),
   NODE_ENV: z.enum(["development", "production"]).default("development"),
   DATABASE_URL: z.string(),
+  BETTER_AUTH_SECRET: z.string(),
+  BETTER_AUTH_URL: z.string(),
+  AWS_ENDPOINT_URL_S3: z.string(),
+  AWS_ACCESS_KEY_ID: z.string(),
+  AWS_SECRET_ACCESS_KEY: z.string(),
+  AWS_REGION: z.string(),
+  AWS_BUCKET_NAME: z.string(),
 });
